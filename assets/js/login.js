d2a6b968c8b3e5eda44c5f5afa8350eec51250a8
likai <likai@192.168.0.16>
2022-05-16 14:46:39 +0800

    登录注册模块切换

---
+++ assets/js/login.js
@@ -0,0 +1,11 @@
+$(function () {
+    $('#link-reg').on('click', function () {
+        $('.reg-box').show()
+        $('.login-box').hide()
+    });
+
+    $('#link-login').on('click', function () {
+        $('.reg-box').hide()
+        $('.login-box').show()
+    })
+})
